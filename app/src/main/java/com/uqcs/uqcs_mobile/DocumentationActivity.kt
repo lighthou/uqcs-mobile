1508f7ed8c283bebec954fccc066073f170be9eb
lighthou <ryankurz123@gmail.com>
2018-11-13 07:37:37 +1000

    [General] Added new activities

---
+++ app/src/main/java/com/uqcs/uqcs_mobile/DocumentationActivity.kt
@@ -0,0 +1,12 @@
+package com.uqcs.uqcs_mobile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DocumentationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_documentation)
+    }
+}
--- app/src/main/java/com/uqcs/uqcs_mobile/MainActivity.kt
+++ app/src/main/java/com/uqcs/uqcs_mobile/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.uqcs.uqcs_mobile
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun onMembersListClick(v: View?) {
+        val membersListIntent = Intent(this, MemberListActivity::class.java)
+        startActivity(membersListIntent)
+    }
+
+    fun onDocumentationClick(v: View?) {
+        val documentationIntent = Intent(this, DocumentationActivity::class.java)
+        startActivity(documentationIntent)
+    }
+
 }
