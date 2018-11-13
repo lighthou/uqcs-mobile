a394b10e79042718c0338c220b1b334bfbb5d952
lighthou <ryankurz123@gmail.com>
2018-11-13 19:05:53 +1000

    [Events Calendar] Can add events on the calendar

--- app/src/main/java/com/uqcs/uqcs_mobile/EventsCalendarActivity.kt
+++ app/src/main/java/com/uqcs/uqcs_mobile/EventsCalendarActivity.kt
@@ -9,21 +9,27 @@ import com.prolificinteractive.materialcalendarview.CalendarDay
 import android.support.annotation.NonNull
 import android.util.EventLog
 import android.util.Log
+import android.view.View
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import org.threeten.bp.LocalDate
 import java.util.*
 
 class EventsCalendarActivity : AppCompatActivity(), OnDateSelectedListener {
 
+    private var currentlySelectedDate : CalendarDay? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events_calendar)
 
         calendarView.setOnDateChangedListener(this)
+        //Set calendar to current date.
+        calendarView.setSelectedDate(LocalDate.now())
+        val c : Calendar = Calendar.getInstance()
+        currentlySelectedDate = CalendarDay.from(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH))
+        dateText.text = resources.getString(R.string.date_format, Util.monthNumberToName(currentlySelectedDate?.month as Int),
+            currentlySelectedDate?.day, currentlySelectedDate?.year)
 
-        var calendarDays : MutableList<CalendarDay> = mutableListOf<CalendarDay>()
-        calendarDays.add(CalendarDay.from(LocalDate.now()))
-        calendarView.addDecorator(EventDecorator(Color.BLACK, calendarDays))
     }
 
     override fun onDateSelected(
@@ -33,7 +39,14 @@ class EventsCalendarActivity : AppCompatActivity(), OnDateSelectedListener {
     ) {
         //If you change a decorate, you need to invalidate decorators
         dateText.text = resources.getString(R.string.date_format, Util.monthNumberToName(date.month), date.day,date.year)
+        currentlySelectedDate = date
         widget.invalidateDecorators()
+    }
+
+    fun addEvent(v : View) {
+        val calendarDays : MutableList<CalendarDay> = mutableListOf<CalendarDay>()
+        calendarDays.add(currentlySelectedDate as CalendarDay)
+        calendarView.addDecorator(EventDecorator(Color.BLACK, calendarDays))
     }
 
 
