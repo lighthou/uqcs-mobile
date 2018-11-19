28691f5df2f43701161a92b6117bcdcd679f5ffa
Ryan Kurz <ryankurz123@gmail.com>
2018-11-19 12:14:08 +1000

    [Events Details] basic dialog class

--- app/src/main/java/com/uqcs/uqcs_mobile/EventDetailsActivity.kt
+++ app/src/main/java/com/uqcs/uqcs_mobile/EventDetailsActivity.kt
@@ -1,12 +1,28 @@
 package com.uqcs.uqcs_mobile
 
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_event_details.*
+import android.R.attr.delay
+import android.app.Activity
+import android.content.Context
 
-class EventDetailsActivity : AppCompatActivity() {
+
+class EventDetailsDialog(var activity: Activity, var event: Event) : Dialog(activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_event_details)
+
+        cancelButton.setOnClickListener { dismiss() }
+        locationText.text = event.location
+        dateText.text = event.date.toString()
+        titleText.text = event.summary
+        descriptionText.text = event.description
     }
 }
+
