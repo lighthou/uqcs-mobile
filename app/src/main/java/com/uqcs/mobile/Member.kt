a5ab91b7da2349c1a2c714626e94ecd077c07dfe
Ryan Kurz <ryankurz123@gmail.com>
2018-11-28 20:25:29 +1100

    [General] using data class because learning about it

--- app/src/main/java/com/uqcs/mobile/Member.kt
+++ app/src/main/java/com/uqcs/mobile/Member.kt
@@ -3,6 +3,6 @@ package com.uqcs.mobile
 import java.text.SimpleDateFormat
 import java.util.*
 
-class Member constructor(var firstName: String, var lastName: String, var email: String, var paid: Boolean) {
+data class Member constructor(var firstName: String, var lastName: String, var email: String, var paid: Boolean) {
 
 }
