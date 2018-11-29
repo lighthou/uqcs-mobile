b23db24704df1809f8328f8514e8f97b103d9203
Ryan Kurz <ryankurz123@gmail.com>
2018-11-29 19:17:27 +1100

    [Members List] Styling changes for searching

--- app/src/main/java/com/uqcs/mobile/MemberListActivity.kt
+++ app/src/main/java/com/uqcs/mobile/MemberListActivity.kt
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.SearchView
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Request
