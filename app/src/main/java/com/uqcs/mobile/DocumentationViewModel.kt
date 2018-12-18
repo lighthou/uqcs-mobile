bd7ba273d44084cdc5eab05bedce43da4830b8d7
Ryan Kurz <ryankurz123@gmail.com>
2018-12-18 21:45:10 +1100

    [Documentation] continued mvvm refactor

--- app/src/main/java/com/uqcs/mobile/DocumentationViewModel.kt
+++ app/src/main/java/com/uqcs/mobile/DocumentationViewModel.kt
@@ -1,17 +1,65 @@
 package com.uqcs.mobile
 
 
+import android.app.Application
+import android.content.Context
+import android.util.Base64
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.uqcs.mobile.data.classes.DocumentationState
 import com.uqcs.mobile.data.classes.DocumentationState.*
 import com.uqcs.mobile.data.classes.Documentation
+import com.uqcs.mobile.data.classes.DocumentationStore
+import kotlinx.android.synthetic.main.activity_documentation.*
+import org.json.JSONObject
 
 
-class DocumentationViewModel : ViewModel() {
+class DocumentationViewModel(application: Application) : AndroidViewModel(application) {
 
     private val DOCUMENTATION_URL = "http://www.ryankurz.me/docs"
-    private lateinit var documentationStore : Documentation
+    private lateinit var documentationStore : DocumentationStore
     private var screenState : DocumentationState = LIST
+    private var keyState = mutableListOf<String>()
+    private lateinit var HTTPAuth : String
+
+    lateinit var observableListItems : List<String>
+    var requestQueue: RequestQueue = Volley.newRequestQueue(getApplication())
+
+    fun getDocumentationFromServer() {
+        requestQueue.add(getDocumentationRequest())
+    }
+
+    fun registerCredentials(username : String, password : String) {
+        val userAndPassword = "$username:$password"
+        HTTPAuth = "Basic " + Base64.encodeToString(userAndPassword.toByteArray(), Base64.NO_WRAP)
+    }
+
+    private fun getDocumentationRequest() : JsonObjectRequest {
+
+        return object : JsonObjectRequest(
+            Request.Method.GET, DOCUMENTATION_URL, null,
+            Response.Listener<JSONObject> { response ->
+                val docs = JSONObject(response.toString())
+                documentationStore = DocumentationStore(docs)
+            },
+            Response.ErrorListener {
+                Log.i("VolleyIssues", it.toString())
+            }) {
+            override fun getHeaders(): Map<String, String> {
+                val params = mutableMapOf<String, String>()
+                params["Authorization"] = HTTPAuth
+                return params
+            }
+        }
+    }
 
 
 
--- app/src/main/java/com/uqcs/mobile/fragments/DocumentationFragmentX.kt
+++ app/src/main/java/com/uqcs/mobile/fragments/DocumentationFragmentX.kt
@@ -2,12 +2,19 @@ package com.uqcs.mobile.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.ListFragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelStore
+import com.uqcs.mobile.DocumentationViewModel
 
 class DocumentationFragmentX : ListFragment() {
 
+    private lateinit var viewModel : DocumentationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        viewModel = ViewModelProviders.of(this).get(DocumentationViewModel::class.java)
+        viewModel.registerCredentials("username", "password")
     }
 }
