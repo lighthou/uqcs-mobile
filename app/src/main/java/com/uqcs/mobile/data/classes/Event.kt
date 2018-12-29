f7fddb720f0ce5fa1bac268965def6008b16043c
Ryan Kurz <ryankurz123@gmail.com>
2018-12-29 19:07:23 +1100

    [General] deleting old files

--- app/src/main/java/com/uqcs/mobile/data/classes/Event.kt
+++
@@ -1,9 +0,0 @@
-package com.uqcs.mobile.data.classes
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-class Event constructor(date: String, var description: String, var summary: String, var location: String) {
-    var format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.UK)
-    var date : Date = format.parse(date)
-}
--- app/src/main/java/com/uqcs/mobile/data/classes/Member.kt
+++
@@ -1,6 +0,0 @@
-package com.uqcs.mobile.data.classes
-
-import java.text.SimpleDateFormat
-import java.util.*
-
-data class Member constructor(var firstName: String, var lastName: String, var email: String, var paid: Boolean) {}
